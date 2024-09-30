fffa1f7f1c50d4b1cb6de846444fe338f79bf8b9
Issam Fladi <80908480+llizuna2001ll@users.noreply.github.com>
2024-09-30 22:43:42 +0100

    Casablance -> Casablanca

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header() {
     return(
           <div className="wrapper">
           <div className="info_container">
-              <li><i className="lni lni-map-marker"></i><strong>47 Av Med Balafrej Lot Ennassr Sidi Moumen, Casablance</strong></li>
+              <li><i className="lni lni-map-marker"></i><strong>47 Av Med Balafrej Lot Ennassr Sidi Moumen, Casablanca</strong></li>
               <li><i className="lni lni-phone"></i><strong>05 22 02 35 05</strong></li>
               <li><i>@</i><strong>safety.vision.k@gmail.com</strong></li>
           </div>
